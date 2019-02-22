80ad869edee67ed027fe557d92bf853d34ba31a0
josdoaitran <josdoaitran@gmail.com>
2019-02-22 16:18:55 +0700

    commit first steps

---
+++ SeleniumBasic/SeleniumBasic/FirstScriptSelenium.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumBasic
+{
+    [TestClass]
+    public class FirstScriptSelenium
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
