3ae1679cf529efd99c63ffca67f5d497412bed2f
josdoaitran <josdoaitran@gmail.com>
2019-02-27 14:48:58 +0700

    update init testing unit

--- SeleniumBasic/SeleniumBasic/FirstScriptSelenium.cs
+++ SeleniumBasic/SeleniumBasic/FirstScriptSelenium.cs
@@ -1,11 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 
 namespace SeleniumBasic
 {
+    [TestClass]
     public class FirstScriptSelenium
     {
+        [TestMethod]
         static void test(string[] args)
         {
             IWebDriver driver = new FirefoxDriver();
