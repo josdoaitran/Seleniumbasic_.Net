34cc412151e5f2d0bbbdef36c6d78798c162c0ba
josdoaitran <josdoaitran@gmail.com>
2019-02-27 15:17:45 +0700

    Update test sccipt

---
+++ SeleniumBasic/UnitTestSelenium/UnitTestSelenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSelenium")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b42f4857-30d7-4782-9316-8b45e61cfb87")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumBasic/UnitTestSelenium/UnitTestSelenium/UnitTestSelenium.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace UnitTestSelenium
+{
+    [TestClass]
+    public class UnitTestSelenium
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://google.com";
+        }
+    }
+}
